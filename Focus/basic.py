d757367bc32be9f6ca9afb9b1d693fecd1ef3b5a
Zo-ZerOs <62164947+Zo-ZerOs@users.noreply.github.com>
2024-06-26 11:54:36 +0700

    Create basic.py

---
+++ Focus/basic.py
@@ -0,0 +1,17 @@
+# output การแสดงผล
+print("IkQ")    # การแสดงผลข้อความ
+print("1")
+
+# comment
+
+# Variable (ตัวแปร)
+a = 5
+b = 3
+print(a)
+
+print(a - b)
+
+# input การรับค่า
+n1 = input()    # input() จะรับค่าเป็น string
+n2 = input()
+print(int(n1) - int(n2))
