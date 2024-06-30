b8c9d9c6a5417da5d64f92aa6eb8b7f472cbafaa
Zo-ZerOs <62164947+Zo-ZerOs@users.noreply.github.com>
2024-06-30 15:01:53 +0700

    Create library.py

---
+++ Best/library.py
@@ -0,0 +1,6 @@
+# buildin function คือ function ที่มีอยู่แล้วในโปรแกรม
+# import function คือ function ที่ต้องนำเข้า เช่น sin(), cos(), tan()
+
+import math # math คือ ชื่อของ library
+
+print(math.pi)
