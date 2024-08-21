e0d6364c02e0f7c10feea08ebe7e98bc74c464dd
Zo-ZerOs <62164947+Zo-ZerOs@users.noreply.github.com>
2024-08-21 20:20:07 +0700

    Create operator.py

---
+++ F/operator.py
@@ -0,0 +1,63 @@
+# opertor (+ - * / ** // % += -=)
+# focus only int and float
+
+# +
+# if there is at least 1 float -> output will be float
+a = 3.2
+b = -5.1
+print(a + b)
+print(type(a + b))
+
+# -
+# if there is at least 1 float -> output will be float
+a = 2.1
+b = 3
+print(a - b)
+print(type(a - b))
+
+# *
+# if there is at least 1 float -> output will be float
+a = 3.5
+b = 10
+print(a * b)
+print(type(a * b))
+
+# /
+# always output as float
+a = 10
+b = 2
+print(a / b)
+print(type(10 / 2))
+
+# **
+# if there is at least 1 float -> output will be float
+a = 4.1
+b = 3.4
+print(a ** b)
+print(type(a ** b))
+
+# // หารไม่เอาเศษ
+# if there is at least 1 float -> output will be float
+a = 10.1
+b = 3.2
+print(a // b)
+print(type(a // b))
+
+# % (modulo) เศษจากการหาร
+# if there is at least 1 float -> output will be float
+a = 10
+b = 3
+print(a % b)
+print(type(a % b))
+
+# += บวกเท่ากับ
+a = 10
+a += 4 # a = a + 4
+
+# -= ลบเท่ากับ
+a = 10
+a -= 4 # a = a - 4
+
+
+
+
