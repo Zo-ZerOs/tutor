fc139928a690c2f0c797ddecdc990c304a3810da
Zo-ZerOs <62164947+Zo-ZerOs@users.noreply.github.com>
2024-08-21 20:18:58 +0700

    Create basic.py

---
+++ F/basic.py
@@ -0,0 +1,30 @@
+# print() function - display data inside () on the screen, and automatically enter to the new line
+
+print(123) # display 123 as a number
+print() # display empty line
+print("Hello") # display Hello as a text
+
+# print without new line
+print("Hello", end = " ")
+print("World", end = "")
+
+# -----------------------------------------------------------------------------------------------
+
+# variable
+
+a = 2 # a is the name and contains 2
+a = "e" # "e" is input to a as a new data
+print(a)
+
+b = 4 # b = 4
+c = 5 # b = 4   c = 5
+b = c # b = 5   c = 5
+c = b # b = 5   c = 5
+
+# -----------------------------------------------------------------------------------------------
+
+# input()
+
+d = input() # input() will wait for the user to enter data
+print(d)
+e = input("PLease enter number: ")
