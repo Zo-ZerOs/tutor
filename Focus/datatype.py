47cc951cb285107d61d9ac3590e69380b8e004b8
Zo-ZerOs <62164947+Zo-ZerOs@users.noreply.github.com>
2024-08-21 20:15:45 +0700

    Update datatype.py

--- Focus/datatype.py
+++ Focus/datatype.py
@@ -2,15 +2,17 @@
 # type = ชนิด
 # datatype = ชนิดของข้อมูล
 
-# 3 ชนิดที่ใช้บ่อยๆ
+# 4 ชนิดที่ใช้บ่อยๆ
 # 1) int (integer = จำนวนเต็ม) -> 5 10 -2 0 -89
 # 2) float (จำนวนจริง) -> 2.38 -5.6 3.0
-# 3) string (ข้อความ) -> "I love pizza" "dog is cat"
+# 3) string (ข้อความ) -> "I love pizza" "dog is cat" "123abc 555!_&@*"
+# 4) boolean (ค่าความจริง) -> True False
 
 a = 3 # int
 b = -10.7 # float
 c = "1" # string
 d = "My grade is 4.00" # string
+e = 3 > 5 # boolean
 
 # e = input() # string
 # f = input()
@@ -20,3 +22,6 @@ d = "My grade is 4.00" # string
 print(float(a)) # เปลี่ยนจาก int เป็น float เติม .0
 print(int(b)) # เปลี่ยนจาก float เป็น int เอาทศนิยมทิ้ง
 print(int(c)) # เปลี่ยนจาก string เป็น int หรือ float    string นั้นๆ ต้องเป็นตัวเลข
+
+# วิธีการดูชนิดของข้อมูล
+print(type(a))
