67c718613a0944e222bcd19fc82ebc2f41c48b14
Zo-ZerOs <62164947+Zo-ZerOs@users.noreply.github.com>
2024-08-24 17:58:12 +0700

    Update string.py

--- F/string.py
+++ F/string.py
@@ -21,8 +21,41 @@ print(B[-3:]) # "024"
 print(B[: 10]) # "Thailand 2"
 print(B[-6:]) # "d 2024"
 
+# ----------------------------------------------------------------------------------
 
+# + and * of string
 
+# + of string (concatenate)
+A = "COM"
+B = "PUTER"
+C = A + B
+print(type(C))
 
+# * of string
+# string * int
+A = "IkQ"
+print(A * 3) # A * 3 = A + A + A
+# if int is 0 or negative, the answer will be empty string
+print(A * -2)
 
+# ----------------------------------------------------------------------------------
+
+# size and loop for string
+
+# to find size of string we use len(name_string)
+A = "F"
+B = len(A)
+print(B)
+
+# loop
+A = "25630"
+
+print(A[0]) # "2"
+print(A[1]) # "5"
+print(A[2]) # "6"
+print(A[3]) # "3"
+print(A[4]) # "0"
+
+for i in range(len(A)): # i = 0, 1, 2, 3, 4
+    print(A[i])
 
