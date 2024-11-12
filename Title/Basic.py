63297182fb23693c8bb34b04035781a09118990f
Zo-ZerOs <62164947+Zo-ZerOs@users.noreply.github.com>
2024-11-12 21:05:12 +0900

    Create Basic.py

---
+++ Title/Basic.py
@@ -0,0 +1,18 @@
+# comment - compiler doesnt read this line
+
+# print() function: print the input to the screen
+print("Hello")
+
+# initailize variable: take data in RHS, and put it into variable in LHS
+x = 2
+
+# input() function: take input from keyboard
+# always be string type
+y = input()
+
+print(str(x) + "Happy")
+
+
+
+
+
