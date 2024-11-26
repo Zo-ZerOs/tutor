6da514a31c945e4e423b542ad435df9200cd1cdf
Zo-ZerOs <62164947+Zo-ZerOs@users.noreply.github.com>
2024-11-26 23:05:19 +0900

    Add files via upload

---
+++ Title/test.py
@@ -0,0 +1,28 @@
+
+# You are given a positive integer N,
+# then you are given a large integer represented as an integer list, where each digits[i] is the ith digit of the integer.
+# You have to print the list contain every digit of the integer but incremented by 1.
+
+# example
+
+# input:    5
+#           3
+#           4
+#           0
+#           1
+#           0 (these 5 digits are representing 34010)
+# output:   [3, 4, 0, 1, 1]
+
+# input:    7
+#           3
+#           0
+#           9
+#           9
+#           9
+#           9
+#           9 (these 7 digits are representing 3099999)
+# output:   [3, 1, 0, 0, 0, 0, 0]
+
+
+
+
